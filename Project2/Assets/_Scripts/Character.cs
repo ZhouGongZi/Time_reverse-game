f9d48ba458a830f086e1b120e74a120342abfac5
YizhouLuo <986732031@qq.com>
2016-02-06 20:41:51 -0500

    sd

---
+++ Project2/Assets/_Scripts/Character.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	//used for movement
+	public float				moveSpeed = 4f;
+	public float				jumpSpeed = 4.5f;
+	public Rigidbody			rigid;
+	//isGrounded
+	public bool 				grounded = false;
+	public int					groundLayerMask;
+	public Vector3 				ray_Offset;
+	public float 				cast_radius = 0.28f; //the length of the ray
+
+	
+	//related to time
+	public int 					time_count = 0;
+
+
+	// Use this for initialization
+	void Start () { 
+		rigid = GetComponent<Rigidbody> ();
+
+		ray_Offset = new Vector3 (0.22f, 0, 0);
+		groundLayerMask = LayerMask.GetMask ("Ground");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//isGrounded 
+		Vector3 center = this.transform.position;
+		grounded = Physics.Raycast (center, Vector3.down, cast_radius, groundLayerMask) 
+			|| Physics.Raycast (center + ray_Offset, Vector3.down, cast_radius, groundLayerMask)
+			|| Physics.Raycast (center - ray_Offset, Vector3.down, cast_radius, groundLayerMask);
+
+		//time runout
+		time_count ++;
+		if (time_count == 550)
+			Destroy (gameObject);
+
+		Vector3 vel = rigid.velocity;
+		//left and right
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			vel.x = moveSpeed;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			vel.x = -moveSpeed;
+		} else {
+			vel.x = 0;
+		}
+		//up and down
+		if (Input.GetKeyDown (KeyCode.S) && grounded) {
+			vel.y = jumpSpeed;
+		}
+
+		rigid.velocity = vel;
+	}
+}
---
+++ Project2/Assets/_Scripts/Origin.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Origin : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject			character_prefab;
+	//related to time
+	public int 					time_count = 0;
+
+	public int 					num_limit = 3;
+	public int 					cur_num = 1;
+
+
+	public static Origin OO; //use singleton tp  
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (num_limit != cur_num) {
+
+			time_count ++;
+			if (time_count % 650 == 0) {
+				GameObject go = Instantiate<GameObject> (character_prefab);
+				Vector3 origin_pos = transform.position;
+				origin_pos.y += 0.25f;
+				go.transform.position = origin_pos;
+				cur_num ++;
+			}
+		}
+
+	}
+}
