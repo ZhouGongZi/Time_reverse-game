ad10cb6f4f87a3af4dcb1c70b46fbc069a2e4b56
YizhouLuo <986732031@qq.com>
2016-02-09 15:38:03 -0500

    asdf

--- Project2/Assets/_Scripts/Origin.cs
+++ Project2/Assets/_Scripts/Origin.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿// simplify the mechanic: 
+// 1. limit the number of characters to be 3
+
+using UnityEngine;
 using System.Collections;
 
 public class Origin : MonoBehaviour {
 
 	// Use this for initialization
-	public GameObject			character_prefab;
+	public GameObject			character_prefab_0;
+	public GameObject			character_prefab_1;
+	public GameObject			character_prefab_2;
 	//related to time
 	public int 					time_count = 0;
 	public int					record_count = 0;
@@ -30,12 +35,12 @@ public class Origin : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (num_limit != cur_num) {
+		if (0 == cur_num) {
 
 			if (time_count % 600 == 0) {
 				cur_num ++;
 
-				GameObject go = Instantiate<GameObject> (character_prefab);
+				GameObject go = Instantiate<GameObject> (character_prefab_0);
 				Vector3 origin_pos = transform.position;
 				origin_pos.y += 0.25f;
 				origin_pos.x += offset;
@@ -43,10 +48,37 @@ public class Origin : MonoBehaviour {
 				go.transform.position = origin_pos;
 			}
 
-		} else if (num_limit == cur_num) {
+		}
+		else if(1 == cur_num){
+			if (time_count % 600 == 0) {
+				cur_num ++;
+				
+				GameObject go = Instantiate<GameObject> (character_prefab_1);
+				Vector3 origin_pos = transform.position;
+				origin_pos.y += 0.25f;
+				origin_pos.x += offset;
+				//offset -= 0.5f;
+				go.transform.position = origin_pos;
+			}
+		}
+		else if(2 == cur_num){
+			if (time_count % 600 == 0) {
+				cur_num ++;
+				
+				GameObject go = Instantiate<GameObject> (character_prefab_2);
+				Vector3 origin_pos = transform.position;
+				origin_pos.y += 0.25f;
+				origin_pos.x += offset;
+				//offset -= 0.5f;
+				go.transform.position = origin_pos;
+			}
+		}
+
+
+		else if (num_limit == cur_num) {
 			if(record_count == 0){
-				G0 = Instantiate<GameObject> (character_prefab);
-				G1 = Instantiate<GameObject> (character_prefab);
+				G0 = Instantiate<GameObject> (character_prefab_0);
+				G1 = Instantiate<GameObject> (character_prefab_1);
 				G0.transform.position = list0[record_count];
 				G1.transform.position = list1[record_count];
 			}else if(record_count <= 549){ //has to be 549 not to over the boundary
